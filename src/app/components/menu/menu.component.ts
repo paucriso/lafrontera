15e259a57c18e543cde5fdd756baa3c40ea5faab
Paulo Otero <paulot73@hotmail.com>
2021-02-17 07:18:11 -0600

    first commit

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,36 @@
+import { MenuService } from './../../services/menu.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  appetizers = [];
+  tacos = [];
+  tortas = [];
+  quesadillas = [];
+  tostadas = [];
+  burritos = [];
+  sopes = [];
+  platillos = [];
+  caldos = [];
+  sideOrders = [];
+  constructor(private menuService: MenuService) {
+    this.appetizers = menuService.appetizers;
+    this.tacos = menuService.tacos;
+    this.tortas = menuService.tortas;
+    this.quesadillas = menuService.quesadillas;
+    this.tostadas = menuService.tostadas;
+    this.burritos = menuService.burritos;
+    this.sopes = menuService.sopes;
+    this.platillos = menuService.platillos;
+    this.caldos = menuService.caldos;
+    this.sideOrders = menuService.sideOrders;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/services/menu.service.ts
@@ -0,0 +1,137 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+  appetizers = [
+    {name: 'Nachos con cualquier carne', price: '$15'},
+    {name: 'Guacamole', price: 'S:$13 L:$18'},
+    {name: 'Mini Taquitos', price: '5x $15'},
+    {name: 'Cocktail de Camarones', price: 'S:$18 L:$25'},
+    {name: 'Mini Quesadillas de Birria', price: '5x $18'},
+    {name: 'Ensalada La Frontera', price: 'S:$9 L:$13'}
+  ];
+
+  tacos = [
+    {name: 'Pollo/Grilled chicken', price: '3x $12'},
+    {name: 'Carne Enchilada/Spicy pork', price: '3x $12'},
+    {name: 'Carnitas/Roast pork', price: '3x $12'},
+    {name: 'Al Pastor/Marinated pork', price: '3x $12'},
+    {name: 'Chorizo/Mexican sausage', price: '3x $12'},
+    {name: 'Oreja', price: '3x $12'},
+    {name: 'Tripa', price: '3x $15'},
+    {name: 'Cecina/Salty beef', price: '3x $15'},
+    {name: 'Bistek/Grilled steak', price: '3x $15'},
+    {name: 'Lengua/Beef tongue', price: '3x $15'},
+    {name: 'Barbacoa', price: '3x $15'},
+    {name: 'Shrimp', price: '3x $15'},
+    {name: 'Chapulines', price: '3x $15'},
+    {name: 'Tacos Placeros', price: '2x $12', extra: 'Tortillas hechas a mano, Arroz, Chile Poblano, Huevo hervido, rajas con epazote de milanesa de pollo o de res.'}
+  ];
+
+  tortas = [
+    {name: 'La Frontera', price: '$13'},
+    {name: 'Carne Enchilada', price: '$10'},
+    {name: 'Cecina', price: '$10'},
+    {name: 'Milanesa de Res', price: '$10'},
+    {name: 'Milanesa de Pollo', price: '$10'},
+    {name: 'Pollo a la Plancha', price: '$10'},
+    {name: 'Carnitas', price: '$10'},
+    {name: 'Chorizo', price: '$10'},
+    {name: 'Oreja', price: '$10'},
+    {name: 'Lengua', price: '$12'},
+    {name: 'Tripa', price: '$12'}
+  ];
+
+  quesadillas = [
+    {name: 'Pollo/Grilled chicken', price: '$15'},
+    {name: 'Bistek/Grilled steak', price: '$15'},
+    {name: 'Carne Enchilada/Spicy pork', price: '$15'},
+    {name: 'Cecina/Salty beef', price: '$15'},
+    {name: 'Tinga/Chipotle chicken', price: '$15'},
+    {name: 'Chorizo/Mexican sausage', price: '$15'},
+    {name: 'Tripa', price: '$15'},
+    {name: 'Lengua/Beef tongue', price: '$15'},
+    {name: 'Flor de Calabaza', price: '$15'},
+    {name: 'Huitlacoche', price: '$15'},
+    {name: 'Champiñones/Mushroom', price: '$15'},
+    {name: 'Chicharrón/ Pork skin', price: '$15'}
+  ];
+
+  tostadas = [
+    {name: 'Pollo/Grilled chicken', price: '$5'},
+    {name: 'Bistek/Grilled steak', price: '$5'},
+    {name: 'Carne Enchilada/Spicy pork', price: '$5'},
+    {name: 'Cecina/Salty beef', price: '$5'},
+    {name: 'Tinga/Chipotle chicken', price: '$5'},
+    {name: 'Carnitas', price: '$5'},
+    {name: 'Al Pastor/Marinated pork', price: '$5'},
+    {name: 'Lengua/Beef tongue', price: '$5'},
+    {name: 'Chorizo/Mexican sausage', price: '$5'},
+    {name: 'Tripa', price: '$5'}
+  ];
+
+  burritos = [
+    {name: 'Pollo/Grilled chicken', price: '$12'},
+    {name: 'Carne Enchilada/Spicy pork', price: '$12'},
+    {name: 'Carnitas/Roast pork', price: '$12'},
+    {name: 'Al Pastor/Marinated pork', price: '$12'},
+    {name: 'Chorizo/Mexican sausage', price: '$12'},
+    {name: 'Oreja/Ear', price: '$12'},
+    {name: 'Cecina/Salty beef', price: '$12'},
+    {name: 'Lengua/Beef tongue', price: '$12'},
+    {name: 'Tripa', price: '$12'},
+  ];
+
+  sopes = [
+    {name: 'Pollo/Grilled chicken', price: '$5'},
+    {name: 'Bistek/Grilled steak', price: '$5'},
+    {name: 'Carne Enchilada/Spicy pork', price: '$5'},
+    {name: 'Cecina/Salty beef', price: '$5'},
+    {name: 'Tinga/Chipotle chicken', price: '$5'},
+    {name: 'Carnitas/Roast pork', price: '$5'},
+    {name: 'Al Pastor/Marinated pork', price: '$5'},
+    {name: 'Lengua/Beef tongue', price: '$5'},
+    {name: 'Chorizo', price: '$5'},
+    {name: 'Oreja', price: '$5'}
+  ];
+
+  platillos = [
+    {name: 'Enchiladas', price: '$17', extra: '(Verdes, Rojas o Mole)'},
+    {name: 'Chilaquiles', price: '$15', extra: '(Verdes o Rojos'},
+    {name: 'Mole Poblano', price: '$19', extra: '(con arroz, frijoles y tortillas)'},
+    {name: 'Camarones a La Mexicana', price: '$23'},
+    {name: 'Camarones a La Diabla', price: '$23'},
+    {name: 'Bistek a La Mexicana', price: '$22'},
+    {name: 'Bistek Encebollado', price: '$21'},
+    {name: 'Ribeye estilo La Frontera', price: '$40'},
+    {name: 'Molcajete La Frontera', price: '$40', extra: 'arroz, frijoles, chorizo, cecina, carne enchilada, pollo y salsa de casa'},
+    {name: 'Plato de Cecina', price: '$21'},
+    {name: 'Plato de Carne Enchilada', price: '$21'},
+    {name: 'Flautas', price: '5x $15', extra: 'Queso, Chorizo o Pollo'}
+  ];
+
+  caldos = [
+    {name: 'Pancita de Res', price: '$15'},
+    {name: 'Pozole', price: '$15'},
+    {name: 'Caldo de Pollo', price: '$15'},
+    {name: 'Chilate de Pollo', price: '$15'},
+    {name: 'Conzomé de Borrego', price: '$15'},
+    {name: 'Mole de Olla', price: '$18'}
+  ];
+
+  sideOrders = [
+    {name: 'Arroz', price: '$5'},
+    {name: 'Frijoles', price: '$5'},
+    {name: 'French Fries', price: '$5'},
+    {name: 'Nopales', price: '$5'},
+    {name: 'Cebollines', price: '$5'},
+    {name: 'Crema', price: '$3'},
+    {name: 'Guacamole', price: '$3'}
+  ];
+
+  
+
+  constructor() { }
+}
